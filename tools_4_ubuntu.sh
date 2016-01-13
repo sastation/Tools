39077f16ec905102d374dd63ce533f221a76e2f0
David Wang <david@family.com>
2016-01-13 13:28:19 +0800

    tools for ubuntu

---
+++ tools_4_ubuntu.sh
@@ -0,0 +1,10 @@
+#!/bin
+
+# clean old kernal
+dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | xargs sudo apt-get -y purge
+
+# list packages size
+dpkg-query -W --showformat='${Installed-Size;10}\t${Package}\n' | sort -k1,1n
+dpkg-query --show --showformat='${Package;-50}\t${Installed-Size}\n' | sort -k 2 -n
+
+
