035323e812c569e770b32f70f1a406dd3f78e4b8
David Wang <sa.station@gmail.com>
2016-12-30 09:58:44 +0800

    count auth.log

--- tools_4_ubuntu.sh
+++ tools_4_ubuntu.sh
@@ -7,4 +7,5 @@ dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/"
 dpkg-query -W --showformat='${Installed-Size;10}\t${Package}\n' | sort -k1,1n
 dpkg-query --show --showformat='${Package;-50}\t${Installed-Size}\n' | sort -k 2 -n
 
-
+# query faild login via sshd
+sudo cat /var/log/auth.log | grep -ia sshd | grep -ia Failed
